6884b0c8d918b84dc380a148aa32472b7c35fb83
Sanat Abdalov <sanatabdalov19@gmail.com>
2025-09-24 11:06:44 -0400

    Convert to Vercel serverless function

---
+++ api/sanatabdalov19_gmail_com.js
@@ -0,0 +1,19 @@
+export default function handler(req, res) {
+  const { x, y } = req.query;
+
+  const isNatural = (n) => /^[1-9]\d*$/.test(n);
+
+  if (!isNatural(x) || !isNatural(y)) {
+    return res.status(200).send('NaN');
+  }
+
+  const gcd = (a, b) => {
+    while (b !== 0) [a, b] = [b, a % b];
+    return a;
+  };
+
+  const lcm = (a, b) => (a * b) / gcd(a, b);
+
+  const result = lcm(Number(x), Number(y));
+  res.status(200).send(result.toString());
+}
